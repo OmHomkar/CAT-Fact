50b37025e31df1a56e560687ba28d3c65008155c
Om Homkar <155510869+OmHomkar@users.noreply.github.com>
2025-01-18 21:54:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const factURL="https://cat-fact.herokuapp.com/facts"; //api link
+const factURL="https://catfact.ninja/fact"; //api link
 
 let btn = document.querySelector("#btn");
 let p = document.querySelector("#p");
@@ -6,8 +6,12 @@ let p = document.querySelector("#p");
 const getFact =  async()=>{
     // fetch API
     let response = await fetch(factURL);
+
     // convert API call information into JS object
     let data = await response.json();
+    
+    console.log(data); //show the output to console browser window.
+
     // print value
     p.innerText=data.fact;
 }
